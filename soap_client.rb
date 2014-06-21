584dc330fec2d2128ad20fae05a90cc0cd7ff2a3
Andrei Cristian Petcu <andrei@ceata.org>
2014-06-21 11:28:44 +0300

    first commit of SOAP client for portal.just.ro

---
+++ soap_client.rb
@@ -0,0 +1,11 @@
+require 'savon'
+
+# create a client for the service
+client = Savon.client(wsdl: 'http://portalquery.just.ro/query.asmx?WSDL')
+client.operations
+response = client.call(:hello_world, message: {})
+puts response.body
+
+response = client.call(:hello_world, message: {institutie: 'CurteadeApelBUCURESTI', dataSedinta: '2013-03-05'})
+puts response.body
+puts response.body[:cautare_sedinte_response][:cautare_sedinte_result][:sedinta][0][:departament]
