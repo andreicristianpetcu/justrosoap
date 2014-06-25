88fc0ea1d1fd4a9dd34826b0f20e64b958540d7a
Andrei Cristian Petcu <andrei@ceata.org>
2014-06-25 22:58:02 +0300

    small fix

--- soap_client.rb
+++ soap_client.rb
@@ -7,5 +7,5 @@ response = client.call(:hello_world, message: {})
 puts response.body
 
 response = client.call(:hello_world, message: {institutie: 'CurteadeApelBUCURESTI', dataSedinta: '2013-03-05'})
-puts response.body
-puts response.body[:cautare_sedinte_response][:cautare_sedinte_result][:sedinta][0][:departament]
+# puts response.body
+euts response.body[:cautare_sedinte_response][:cautare_sedinte_result][:sedinta][0][:departament]
