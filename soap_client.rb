e48c67125c030a3660bae4e89794158da03120fb
Andrei Cristian Petcu <andrei@ceata.org>
2014-06-26 00:22:46 +0300

    small fix

--- soap_client.rb
+++ soap_client.rb
@@ -1,3 +1,6 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
 require 'savon'
 
 # create a client for the service
@@ -6,6 +9,6 @@ client.operations
 response = client.call(:hello_world, message: {})
 puts response.body
 
-response = client.call(:hello_world, message: {institutie: 'CurteadeApelBUCURESTI', dataSedinta: '2013-03-05'})
-# puts response.body
-euts response.body[:cautare_sedinte_response][:cautare_sedinte_result][:sedinta][0][:departament]
+response = client.call(:cautare_sedinte, message: {institutie: 'CurteadeApelBUCURESTI', dataSedinta: '2013-03-05'})
+puts response.body
+puts response.body[:cautare_sedinte_response][:cautare_sedinte_result][:sedinta][0][:departament]
